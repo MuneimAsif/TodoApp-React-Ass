d7482c97e12b29cf6c722d9e4272f9b85308a79f
MuneimAsif <127970035+MuneimAsif@users.noreply.github.com>
2023-08-19 18:48:59 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import  { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import './App.css'; // Import your CSS if needed
 
 
